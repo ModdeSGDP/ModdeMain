8e6f4b030ff7e58e44fb158fa4cbd2d7cdc73aba
Kalindukw <108806927+Kalindukw@users.noreply.github.com>
2025-01-21 13:36:30 +0530

    Feat:create-product dto

---
+++ backend/src/product/dtos/create-product.dto.ts
@@ -0,0 +1,23 @@
+import { IsString, IsNotEmpty, IsNumber } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  price: number;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsString()
+  @IsNotEmpty()
+  category: string;
+
+  @IsString()
+  @IsNotEmpty()
+  organizationId: string;
+}
