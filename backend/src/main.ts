a08207e0a70d6eb94e01eaefc49d9d7ab6e1570c
ransicka <kransika27@gmail.com>
2025-01-25 11:05:04 +0530

    Update: User schema, dto User schema was lacking fields.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+      whitelist: true, // Removes extra properties not defined in the DTO
+      forbidNonWhitelisted: true, // Throws an error if unknown properties are sent
+      transform: true, // Automatically transforms payloads into DTO instances
+  }));
   await app.listen(3000);
 }
 bootstrap();
