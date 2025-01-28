202dd9fb380db83f7651159b132a837831f19f79
Kalindukw <108806927+Kalindukw@users.noreply.github.com>
2025-01-28 22:18:06 +0530

    Refactor:update controller and module

--- backend/src/product/product.module.ts
+++ backend/src/product/product.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import {ProductController} from './product.controller'
+import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
 import { Product, ProductSchema } from './schema/product.schema';
-
+import { S3Service } from '../common/aws/s3.service'; // Import the S3Service
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'Product', schema: ProductSchema }])],
   controllers: [ProductController],
-  providers: [ProductService],
+  providers: [ProductService, S3Service], // Register S3Service as a provider
   exports: [ProductService],
 })
 export class ProductModule {}
